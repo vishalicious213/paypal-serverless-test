89b5bebe70daec2d58cd88c6122c0a2642f68a68
vishalicious213 <vishalicious213@gmail.com>
2025-02-12 18:13:10 -0800

    added env variables to hello.js for testing

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,12 @@
 exports.handler = async event => {
     const name = event.queryStringParameters.name || 'World'
+    const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env
     return {
         statusCode: 200,
-        body: `Hello ${name}!`,
+        body: `
+        Hello ${name}!
+        Your client ID is ${PAYPAL_CLIENT_ID}.
+        Your secret is ${PAYPAL_CLIENT_SECRET}.
+        `,
     }
 }
