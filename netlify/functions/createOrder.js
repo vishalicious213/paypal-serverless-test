896448ffe5c355056f0608b55acf052b31be8e0a
vishalicious213 <vishalicious213@gmail.com>
2025-02-14 02:20:11 -0800

    /functions/createOrder: moved getAccessToken logic into createOrder & made it a function

--- netlify/functions/createOrder.js
+++ netlify/functions/createOrder.js
@@ -1,6 +1,29 @@
 exports.handler = async event => {
+    const { PAYPAL_BASEURL, PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env
+
+    async function getAccessToken() {
+        try {
+            const response = await fetch(`${PAYPAL_BASEURL}/v1/oauth2/token`, {
+                method: "POST",
+                form: {
+                    grant_type: "client_credentials",
+                },
+                username: PAYPAL_CLIENT_ID,
+                password: PAYPAL_CLIENT_SECRET,
+            })
+            console.log(response.body)
+            const data = JSON.parse(response.body)
+            const newAccessToken = data.access_token
+            return newAccessToken
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
     try {
         // const accessToken = await fetch("/.netlify/functions/getAccessToken")
+        const accessToken = await getAccessToken()
+
         return {
             statusCode: 200,
             body: JSON.stringify({
